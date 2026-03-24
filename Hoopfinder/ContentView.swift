563814be9ded55621950f7cbd800d8fc5402b54f
Shreyas Mahendra <shreyas.mahendra17@gmail.com>
2026-03-23 20:10:13 -0700

    Initial Commit

---
+++ Hoopfinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hoopfinder
+//
+//  Created by Shreyas Mahendra on 3/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hoopfinder/HoopfinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopfinderApp.swift
+//  Hoopfinder
+//
+//  Created by Shreyas Mahendra on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct HoopfinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
